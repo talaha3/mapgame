6de8dea23836c63d763330975f78a8b1e8a4591a
Talaha Modak <talahamodak@gmail.com>
2023-06-15 12:15:05 +0100

    creating different gui for desktop and mobile

--- priv.js
+++ priv.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const Datastore = require('nedb')
 const app = express();
+
+
+
 app.listen(3000, () => console.log('Listening at 3000'));
 
 app.use(express.static('public'));
@@ -7,13 +11,16 @@ app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 app.use(express.text());
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response)=>{
   //console.log('I got a request');
   console.log(request.body);
+  database.insert({word: request.body})
   response.json({
     status: "success"
   }
-
   )
 });
 
--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,28 @@
-keys = [];
-output = [];
-rectwidth = 0;
-textx = 0;
-texty = 0;
-texth = 0 ;
-textw = 0;
-butpressed = -1;
-presscheck = false;
-word = '';
-letters = 'qwertyuiopasdfghjklzxcvbnm'.split('')
-let flag;
+keys = []; //stores keyboard keys
+output = []; //stores typed output
+
+rectwidth = 0; //stores width of keyboard keys
+
+textx = 0; //x-coordinate of textbox
+texty = 0; //y-coordinate of textbox
+texth = 0 ; //height of textbox
+textw = 0; //width of textbox
+
+butpressed = -1; //stroes which button is being pressed
+presscheck = false; //checks if button is being pressed
+
+word = ''; //stores the word that is entered
+
+letters = 'qwertyuiopasdfghjklzxcvbnm'.split('') //qwerty keyboard
+
+let flag; //stores flag image
+flagy = 0;
+flagx = 0;
+scoreboxy = 0;
+scoreboxx = 0;
+scoretexty = 0;
+scorenumy = 0;
+
 
 
 
@@ -25,8 +38,16 @@ function setup() {
 		cnv = createCanvas(0.8*windowHeight, 0.9*windowHeight)
 	}
 	background(137, 7, 176);
-	textFont('Varela Round')
-	makekeys();
+	textFont('Varela Round');
+
+	makeDesktopKeys();
+	desktopFlagCords();
+
+	//makeMobileKeys();
+	//mobileFlagCords();
+
+	flagx = width/2;
+	scoreboxx = width/16;
 	drawflag();
 	
 
@@ -40,21 +61,20 @@ function draw() {
 	keyboard();
 	if(presscheck&&frameCount<10){
 		for(k of keys){
-			if (k.text == butpressed){
-				key = k;
+			if (k.text === butpressed){
+				fill(70)
+				rectMode(CENTER);
+				stroke(0);
+				strokeWeight(1);
+				rect(k.x, k.y, k.width, k.height, 5);
+				fill(255)
+				textSize(rectwidth/2)
+				textStyle(BOLD)
+				textAlign(CENTER, CENTER);
+				noStroke();
+				text(k.text, k.x, k.y)
 			}
 		}
-		fill(70)
-		rectMode(CENTER);
-		stroke(0);
-		strokeWeight(1);
-		rect(key.x, key.y, key.width, key.height, 5);
-		fill(255)
-		textSize(rectwidth/2)
-		textStyle(BOLD)
-		textAlign(CENTER, CENTER);
-		noStroke();
-		text(key.text, key.x, key.y)
 	}
 	else{
 		presscheck=false;
@@ -66,7 +86,7 @@ function draw() {
 	// put drawing code here
 }
 
-function makekeys(){
+function makeMobileKeys(){
 	rectMode(CENTER);
 	stroke(0);
 	strokeWeight(1);
@@ -81,6 +101,8 @@ function makekeys(){
 	rectwidth = (width-(0.1*width)-(0.02*width))/10;
 	rectheight = (height - 5*height/9-1.5*0.11*width/2-0.05*height)/4
 
+	fill(248,235,254);
+	rect(width/2, 15*height/18, width, 5*height/9-1.5*0.11*width); //keyboard background
 	
 	for(let i = 0; i<26; i++){ //adds keys
 	if(i<10){
@@ -144,13 +166,44 @@ function makekeys(){
 	
 }
 
+function makeDesktopKeys(){
+	rectMode(CENTER);
+	stroke(0);
+	strokeWeight(1);
+	textx = width/2;
+	textw = 0.8*width;
+	texth = 0.1*width;
+	texty = 13*height/18;
+
+	fill(255);
+	rect(textx, texty, textw, texth, 3); //text area
+
+	rectwidth = (width-(0.1*width)-(0.02*width))/10;
+
+
+	let ent = {
+		x: width/2,
+		y: texty+2*height/18,
+		width: width-textw, 
+		height: texth,
+		fill: 2,
+		text: 'enter'
+	}
+
+	keys.push(ent); //enter button
+
+
+
+
+
+}
+
 function keyboard(){
 	rectMode(CENTER);
 	stroke(0);
 	strokeWeight(1);
 
-	fill(248,235,254);
-	rect(width/2, 15*height/18, width, 5*height/9-1.5*0.11*width); //keyboard background
+	
 	for(key of keys){ //draws keys
 		if(key.fill==2){
 			fill(56, 192, 70)		
@@ -260,7 +313,6 @@ function keyPressed(){
 		presscheck = true;
 		if(output.length>0){
 			sendword(txt);
-			
 			output = [];
 		}
 	}
@@ -280,21 +332,49 @@ function keyPressed(){
 
 }
 
+function mobileFlagCords(){
+	flagy = 2*height/7
+	scoreboxy = 2*height/7
+	scoretexty = 2*height/7-height/30;
+	scorenumy = 2*height/7+height/20;
+	roundy = height/10
+}
+
+function desktopFlagCords(){
+	flagy = 2*height/5
+	scoreboxy = 2*height/5
+	scoretexty = 2*height/5-height/30;
+	scorenumy = 2*height/5+height/20;
+	roundy = width/6
+}
+
 function drawflag(){
+	rectMode(CENTER)
 	flagw = flag.width/(flag.width+flag.height)
 	flagh = flag.height/(flag.width+flag.height)
 	imageMode(CENTER);
 	stroke(255);
 	strokeWeight(10);
-	rect(width/2, 2*height/7, width*flagw, width*flagh,4)
-	image(flag, width/2, 2*height/7, width*flagw, width*flagh);
+	rect(flagx, flagy, width*flagw, width*flagh,4)
+	image(flag, flagx, flagy, width*flagw, width*flagh);
 	fill(255);
 	textAlign(CENTER);
 	textStyle(BOLD);
 	textSize(width/10)
 	noStroke();
-	text('Round 1', width/2, height/10)
+	text('Round 1', width/2, roundy)
 	textStyle(NORMAL);
+	fill(255,195,0);
+	rect(scoreboxx, scoreboxy, width/8, height/7,8)
+	rect(15*scoreboxx, scoreboxy, width/8, height/7,8)
+	fill(199,0,57)
+	textSize(width/25);
+	text('You', scoreboxx, scoretexty);
+	text('Opp', 15*scoreboxx, scoretexty);
+	textSize(width/10);
+	text('0', scoreboxx, scorenumy)
+	text('0', 15*scoreboxx, scorenumy)
+
 }
 
 async function sendword(word){
